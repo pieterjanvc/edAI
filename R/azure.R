4dfe865736d8cab09644d5f41c90e172c60a25fe
pieterjanvc <pieterjanvc@hotmail.com>
2026-02-20 11:13:00 -0500

    Impelement setup and basic chat
    - Use a Shiny app to get credentials
    - rename functions
    - Implement credential checks
    - Update docs
    - Add first vignette

--- R/azure.R
+++ R/azure.R
@@ -1,34 +1,105 @@
-#' Setting up the Azure API info and storing it in the keyring
+#' Interactive app to set Azure credentials
 #'
-#' @param username Username can be anything. It's used to distinguish between
-#' users who share an API key
-#' @param force_overwrite (Default  = FALSE) Force reset of the parameters
-#' @param key (Optional) Azure API key. Pop-up will show if not provided
+#' @import shiny bslib
 #'
-#' @import keyring
+#' @returns Starts a Shiny app that returns username and key
 #'
-#' @returns Message if success
-#' @export
+cred_app <- function() {
+  runApp(shinyApp(
+    ui <- page_fluid(
+      titlePanel("Azure OpenAI Credentials"),
+      textInput("username", "Pick a username"),
+      tags$i(
+        "The username can be anything, but is used to track your token usage"
+      ),
+      hr(),
+      textInput("key", "Provide an Azure API Key"),
+      actionButton("submit", "Submit")
+    ),
+
+    server <- function(input, output, session) {
+      observe({
+        stopApp(list(username = input$username, key = input$key))
+      }) |>
+        bindEvent(input$submit)
+    }
+  ))
+}
+
+#' Validate Azure API key
 #'
-azure_openai_setup <- function(
-  username,
-  force_overwrite = F,
-  key
-) {
-  if (nrow(key_list("AZURE_OPENAI_USERNAME")) != 1) {
-    key_set_with_value("AZURE_OPENAI_USERNAME", password = username)
+#' @param key API / Token to check
+#' @param error_on_fail (Default = F) Generate error when failed authentication
+#'
+#' @import httr2
+#' @importFrom dplyr case_when
+#'
+#' @returns List with checks
+#'
+cred_check <- function(key, error_on_fail = F) {
+  status_code <- request(
+    "https://azure-ai.hms.edu/openai/"
+  ) |>
+    req_headers(
+      "Content-Type" = "application/json",
+      "api-key" = key
+    ) |>
+    req_error(is_error = \(resp) FALSE) |>
+    req_perform() |>
+    resp_status()
+
+  msg <- case_when(
+    status_code == 401 ~ "The provided key is not valid (anymore)",
+    status_code == 404 ~ "Successful authentication",
+    TRUE ~ "There is an issue with the Azure server. Try again later"
+  )
+
+  if (error_on_fail & status_code != 404) {
+    stop(msg)
   }
 
-  if (nrow(key_list("AZURE_OPENAI_API_KEY")) != 1) {
-    if (missing(key)) {
-      key_set(
-        "AZURE_OPENAI_API_KEY",
-        prompt = "Please copy-paste your Azure API key"
-      )
-    } else {
-      key <- key_set_with_value("AZURE_OPENAI_API_KEY", password = key)
-    }
+  return(invisible(list(
+    success = status_code == 404,
+    status_code = status_code,
+    msg = msg
+  )))
+}
+
+#' Setting up the Azure API info and storing it in the keyring
+#'
+#' @param force_overwrite (Default  = FALSE) Force reset of the parameters
+#' @param username (Optional if interactive) Username can be anything.
+#' It's used to distinguish between users who share an API key
+#' @param key (Optional if interactive) Azure API key
+#'
+#' @import keyring
+#' @importFrom jsonlite toJSON
+#'
+#' @returns Message
+#' @export
+#'
+edAI_setup <- function(
+  force_overwrite = F,
+  username,
+  key
+) {
+  if (!force_overwrite & nrow(key_list("edAI_creds")) == 1) {
+    message(
+      "Everything is set up already. You can remove this function from your code ",
+      "or set `force_overwrite = T` if you need to update your key."
+    )
+    return(invisible())
   }
+
+  if (interactive()) {
+    info <- cred_app()
+  } else {
+    info <- list(username = usename, key = key)
+  }
+
+  cred_check(info$key, error_on_fail = T)
+
+  key_set_with_value("edAI_creds", password = toJSON(info))
 
   message("Everything was set up and stored for future use")
 }
@@ -38,24 +109,46 @@ azure_openai_setup <- function(
 #' When the session ends, token usage info will be uploaded to a Google Drive
 #'
 #' @import httr2 keyring
+#' @importFrom jsonlite fromJSON
 #'
 #' @returns Message
 #' @export
 #'
-azure_openai_init <- function() {
-  if (nrow(key_list("AZURE_OPENAI_API_KEY")) != 1) {
-    stop("Azure info not set, please run azure_openai_setup()")
+edAI_init <- function() {
+  if (nrow(key_list("edAI_creds")) != 1) {
+    stop("Credentials not found, please run edAI_setup()")
   }
 
-  Sys.setenv("AZURE_OPENAI_API_KEY" = key_get("AZURE_OPENAI_API_KEY"))
+  info <- key_get("edAI_creds") |> fromJSON()
+
+  if (Sys.getenv("AZURE_OPENAI_API_KEY") == info$key) {
+    message("Everything has been set up already")
+    return(invisible())
+  }
+
+  check <- cred_check(info$key)
+
+  if (!check$success) {
+    stop(
+      "You no longer have access to Azure with the credentials provided ",
+      "when you installed the edAI package and should stop using the ",
+      "edAI_setup() and edAI_init() functions, but can continue using other ",
+      "functions and the `ellmer` library if you get new credentials. ",
+      "Check the ellmer documentation for more info at ",
+      " https://ellmer.tidyverse.org/reference/chat_azure_openai.html?q=api%20key#authentication"
+    )
+  }
+
+  Sys.setenv("AZURE_OPENAI_API_KEY" = info$key)
   Sys.setenv("AZURE_OPENAI_ENDPOINT" = "https://azure-ai.hms.edu")
 
   # Save the token info when ending the session
   suppressMessages(withr::defer(
     {
+      # Only run when there has been token usage
       if (nrow(token_usage()) > 0) {
         filename <- paste0(
-          key_get("AZURE_OPENAI_USERNAME"),
+          info$username,
           "_",
           as.integer(Sys.time()),
           ".csv"
@@ -76,5 +169,5 @@ azure_openai_init <- function() {
     priority = "last"
   ))
 
-  message("You are ready to start using the Azure OpenAI models")
+  message("You are ready to start using LLMs!")
 }
--- dev/dev.R
+++ dev/dev.R
@@ -1,41 +1,42 @@
 library(ellmer)
-library(keyring)
-library(httr2)
 
-azure_init()
+edAI_init()
 
 
-library(ellmer)
 chat <- chat_azure_openai(
   model = "gpt-5-mini",
-  system_prompt = "be academic in response"
+  system_prompt = "Be terse"
 )
+
+test <- chat$chat("what is the month now")
+
 # chat$register_tool(openai_tool_web_search())
 
 # Define a regular function to use as a tool
-get_current_time <- function(tz = "UTC") {
-  format(Sys.time(), tz = tz, usetz = TRUE)
+lucky_numbers <- function(n = 3) {
+  if (n < 1) {
+    return("You must request at least 1 number")
+  }
+  set.seed(Sys.Date())
+  sample(1:100, n)
 }
 
 # Create the tool
-get_current_time <- tool(
-  get_current_time,
-  name = "get_current_time",
-  description = "Returns the current time.",
+lucky_numbers <- tool(
+  lucky_numbers,
+  name = "lucky_numbers",
+  description = "Returns a list of n lucky numbers (changes daily). Defaults to 3",
   #https://ellmer.tidyverse.org/articles/tool-calling.html#tool-inputs-and-outputs
   arguments = list(
-    tz = type_string(
-      "Time zone to display the current time in. Defaults to `\"UTC\"`.",
-      required = FALSE
-    )
+    n = type_integer("The number of lucky numbers to return")
   )
 )
 
 # Register the tool
-chat$register_tool(get_current_time)
+chat$register_tool(lucky_numbers)
 
-chat$chat("what is the month now")
+chat$chat("Can I have 0 lucky numbers?")
+
+chat$chat("2")
 
 token_usage()
-
-chat$get_tokens()
